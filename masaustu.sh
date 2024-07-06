044b84c74b7a59aa2b7c661f67b4f142dccb0232
Xavia <98714294+xavianoan@users.noreply.github.com>
2024-07-06 19:34:41 +0300

    Create masaustu.sh

---
+++ masaustu.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+    slm=$(echo "\033[31;3m")
+    normal=$(echo "\033[m")
+    menu=$(echo "\033[36m") #Blue
+    number=$(echo "\033[33m") #yellow
+    bgred=$(echo "\033[39;3m")
+    fgred=$(echo "\033[32;3m")
+    kbred=$(echo "\033[33;3m")
+    printf "\n${kbred}-----> Kurulum ilerliyor${normal}\n"
+
+
+sudo apt update
+
+sudo apt -y install xrdp
+
+sudo adduser xrdp ssl-cert
+
+sudo ufw allow 3389
+
+sudo systemctl restart xrdp
+
+
+dsrt=$(hostnamectl | egrep "Operating System" | cut -f2 -d":" | cut -f2 -d " ")
+system3=$(hostnamectl | egrep "Operating System" | cut -f2 -d":" | cut -f2-4 -d " ")
+if [[ $system3 = "Debian GNU/Linux 9" ]] ; then
+sudo apt -y install task-lxqt-desktop
+else
+if [[ $dsrt = "Debian" ]] ; then
+sudo apt-get -y install task-gnome-desktop
+elif [[ $dsrt = "Ubuntu" ]] ; then
+sudo apt -y install xfce4
+echo xfce4-session >~/.xsession 
+sudo service xrdp restart
+fi
+fi
+clear && printf '\e[3J'
+echo ""
+echo ""
+echo ""
+echo "############################################"
+ip_add=$(wget -qO- ifconfig.me)
+echo -e "\033[31;4m##  İp Adresiniz: $ip_add\033[0m"
+echo "############################################"
+echo ""
+
+    printf "\n${slm}-----> Kurulum Bitti....${normal}\n"
