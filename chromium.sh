9b3a841ce20e5cd21b5dc52bb47d5d00f7ac57ef
Xavia <98714294+xavianoan@users.noreply.github.com>
2024-10-28 06:15:17 +0300

    Create chromium.sh

---
+++ chromium.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+mkdir -p chromium && cd chromium && echo "services:
+  chromium:
+    image: lscr.io/linuxserver/chromium:latest
+    container_name: chromium
+    security_opt:
+      - seccomp:unconfined #optional
+    environment:
+      - CUSTOM_USER=randomkullanıcıadı
+      - PASSWORD=random
+      - PUID=1000
+      - PGID=1000
+      - TZ=Europe/London
+      - CHROME_CLI=
+    volumes:
+      - /root/chromium/config:/config
+    ports:
+      - 3010:3000 
+      - 3011:3001
+    shm_size: '1gb'
+    restart: unless-stopped" > docker-compose.yaml && docker compose up -d
